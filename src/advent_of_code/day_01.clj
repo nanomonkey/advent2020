fc71ec257e725902a2f8d6ec61b06acc3d733682
nanomonkey <scottrobertgarrison@gmail.com>
2020-12-02 00:10:03 -0800

    First two days done

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,11 +1,23 @@
-(ns advent-of-code.day-01)
+(ns advent-of-code.day-01
+  (:require [clojure.string :as str]
+            [clojure.java.io :refer [resource]])) 
+
+(def input (map #(Integer/parseInt %) (str/split-lines (slurp (resource "day-01.txt")))))
 
 (defn part-1
-  "Day 01 Part 1"
   [input]
-  input)
+  (for [x input
+          y input
+          :let [z (+ x y)]
+          :when (and (not= x y) (= 2020 z))]
+      (* x y)))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  input)
+  (for [x input
+        y input
+        z input
+        :let [w (+ x y z)]
+        :when (and (not= x y z) (= 2020 w))]
+    (* x y z)))
--- src/advent_of_code/day_02.clj
+++ src/advent_of_code/day_02.clj
@@ -1,11 +1,34 @@
-(ns advent-of-code.day-02)
+(ns advent-of-code.day-02
+    (:require [clojure.string :as str]
+              [clojure.java.io :refer [resource]]))
+
+(def input (str/split-lines (slurp (resource "day-02.txt"))))
+
+(defn parse [line]
+  (re-find #"(\d+)-(\d+) (\S): (\S+)" line))
 
 (defn part-1
   "Day 02 Part 1"
   [input]
-  input)
+  (count (for [x input
+               :let [[_ min-str max-str letter password] (parse x)
+                     min (Integer/parseInt min-str)
+                     max (Integer/parseInt max-str)
+                     cnt ((frequencies password) (first (seq letter)))]
+               :when (and cnt 
+                          (>= cnt min)
+                          (<= cnt max))]
+           _)))
 
 (defn part-2
   "Day 02 Part 2"
   [input]
-  input)
+  (count (for [x input
+               :let [[_ min-str max-str letter-str password] (parse x)
+                     letter (first (seq letter-str))
+                     min (Integer/parseInt min-str)
+                     max (Integer/parseInt max-str)
+                     loc1? (= (nth password (- min 1)) letter)
+                     loc2? (= (nth password (- max 1)) letter)]
+               :when (and (or loc1? loc2?) (not= loc1? loc2?))]
+           _)))
