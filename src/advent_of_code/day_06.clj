5e6b4c3cc61ecf7c105236807851693cfdfe44c5
nanomonkey <scottrobertgarrison@gmail.com>
2020-12-05 23:12:29 -0800

    Day 6

--- src/advent_of_code/day_06.clj
+++ src/advent_of_code/day_06.clj
@@ -1,11 +1,45 @@
-(ns advent-of-code.day-06)
+(ns advent-of-code.day-06
+ (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [clojure.edn :as edn]
+            [clojure.set :as set]))
+
+(def file (slurp (io/resource "day-06.txt")))
+(defn seperate [file] (str/split file #"\n\n"))
+(def example "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+")
 
 (defn part-1
   "Day 06 Part 1"
   [input]
-  input)
+  (reduce + (for [group (seperate input)]
+              (count (disj (set group) \newline)))))
+
 
 (defn part-2
   "Day 06 Part 2"
   [input]
-  input)
+  (reduce + (for [group (seperate input)]
+              (count (apply set/intersection (for [person (str/split-lines group)]
+                                               (set person)))))))
+
+
+(comment
+(part-2 file)
+(part-2 example)
+)
