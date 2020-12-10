f9b031f59096413aa570a0b7f4d202d21d272a2a
nanomonkey <scottrobertgarrison@gmail.com>
2020-12-09 21:53:06 -0800

    Day 7 and 8

--- src/advent_of_code/day_07.clj
+++ src/advent_of_code/day_07.clj
@@ -1,11 +1,78 @@
-(ns advent-of-code.day-07)
+(ns advent-of-code.day-07
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [clojure.edn :as edn]
+            [clojure.set :as set]))
 
-(defn part-1
-  "Day 07 Part 1"
-  [input]
-  input)
+(def file (slurp (io/resource "day-07.txt")))
 
-(defn part-2
-  "Day 07 Part 2"
-  [input]
-  input)
+(defn seperate [file] (str/split file #"\.\n"))
+
+(def example "light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.")
+
+(defn parse-contains [contains] 
+  (into {}
+        (for [item (str/split contains #", ")
+              :let [[_ num color] (re-find  #"(\d+|no) (\w* \w*) bag" item)]]
+          {color num})))
+
+(defn parse-description [description]
+  (let [[color contains] (str/split description #" bags contain ")]
+       {color (parse-contains contains)}))
+
+(defn bags-that-hold [color map]
+  (let [bags (atom #{})]
+    (doall
+     (for [[k v] map
+           :when (and (contains? v color)
+                      (not= {nil nil} (get-in v [color :num])))]
+       (when (not (contains? @bags k)) 
+         (do (swap! bags conj k)
+             (doseq [x (bags-that-hold k map)] (swap! bags conj x) )))))
+    @bags))
+
+(defn part-1 [input]
+  "How many bag colors can eventually contain at least one shiny gold bag?"
+  (let [bag-map (into {} (for [description (seperate input)]
+                         (parse-description description)))] 
+    (count (bags-that-hold "shiny gold" bag-map))))
+
+
+(defn count-bags [color tree]
+  (let [accum
+        (reduce (fn [cnt [bag num]] 
+                  (if (string? num) 
+                    (+ cnt (* (read-string num) (count-bags bag tree)))
+                    1))
+                1
+                (tree color))]
+    accum))
+
+(defn part-2 [input]
+  "How many individual bags are required inside your single shiny gold bag?"
+  (let [bag-dependencies (into {} (for [description (seperate input)]
+                                    (parse-description description)))] 
+    (dec (count-bags "shiny gold" bag-dependencies))))
+
+(def example2 "shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.")
+
+(comment
+ (part-2 example) ;; 32
+(part-2 example2) ;; 126
+(part-2 file)
+)
--- src/advent_of_code/day_08.clj
+++ src/advent_of_code/day_08.clj
@@ -1,11 +1,75 @@
-(ns advent-of-code.day-08)
+(ns advent-of-code.day-08
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [clojure.edn :as edn]))
+
+(def file (slurp (io/resource "day-08.txt")))
+
+(defn get-instructions [input] (for [line (str/split-lines input) 
+                                     :let [[operation arg] (str/split line #" ")]]
+                                 [operation (read-string arg)]))
+
+(def example "nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6")
+
+(-> example
+    get-instructions)
 
 (defn part-1
   "Day 08 Part 1"
   [input]
-  input)
+  (let [instructions (get-instructions input)
+        accumulator (atom 0)
+        position (atom 0)
+        visited (atom #{})]
+    (while (not (@visited @position))
+      (do (swap! visited conj @position)
+          (let [[operation arg] (nth instructions @position)]
+            (case operation
+              "acc" (do (swap! accumulator #(+ arg %))
+                        (swap! position inc))
+              "nop" (swap! position inc)
+              "jmp" (swap! position #(+ arg %))))))
+    @accumulator))
+
+(comment
+(part-1 example)
+(part-1 file)
+)
 
 (defn part-2
   "Day 08 Part 2"
   [input]
-  input)
+  (let [instructions (into [] (get-instructions input))
+        instruct-cnt (count instructions)]
+    (filter some? (for [pos (range  instruct-cnt)
+                        :when (not= "acc" (first (nth instructions pos)))
+                        :let [[inst arg] (nth instructions pos)
+                              new-instr (assoc instructions pos 
+                                               [(if (= inst "jmp") "nop" "jmp") arg])]]
+                    (let [accumulator (atom 0)
+                          position (atom 0)
+                          visited (atom #{})]
+                      (while (and (< @position instruct-cnt) (not (@visited @position)))
+                        (do (swap! visited conj @position)
+                            (let [[operation arg] (nth new-instr @position)]
+                              (case operation
+                                "acc" (do (swap! accumulator #(+ arg %))
+                                          (swap! position inc))
+                                "nop" (swap! position inc)
+                                "jmp" (swap! position #(+ arg %))))))
+                      (when (>= @position instruct-cnt) @accumulator))))))
+
+(comment
+  (part-2 example)
+  (part-2 file)
+)
+
