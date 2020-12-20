09f0fa952e54c219cc060009b62579f76d7f7025
nanomonkey <scottrobertgarrison@gmail.com>
2020-12-19 21:56:03 -0800

    Day 13 part 1

--- src/advent_of_code/day_13.clj
+++ src/advent_of_code/day_13.clj
@@ -1,10 +1,34 @@
-(ns advent-of-code.day-13)
+(ns advent-of-code.day-13
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(def file (slurp (io/resource "day-13.txt")))
+
+(defn parse [input]
+  (let [[earliest buses] (str/split-lines input)]
+    [(read-string earliest) 
+     (map read-string (filter #(not= "x" %) (str/split buses #",")))]))
+
+(def example "939
+7,13,x,x,59,x,31,19")
+
+(let [[earliest buses] (-> example parse)
+      bus (first buses)]
+  (- bus (mod 939 bus)))
 
 (defn part-1
   "Day 13 Part 1"
   [input]
-  input)
+  (let [[earliest buses] (parse input)]
+    (apply *
+           (apply min-key second
+                  (for [bus buses]
+                    [bus (- bus (mod earliest bus))])))))
 
+(comment
+(part-1 example)
+(part-1 file)
+)
 (defn part-2
   "Day 13 Part 2"
   [input]
