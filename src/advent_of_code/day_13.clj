b5f98d95dd818b77edfa10f550880f18d859a225
nanomonkey <scottrobertgarrison@gmail.com>
2020-12-23 10:38:25 -0800

    Day 13 part 2

--- src/advent_of_code/day_13.clj
+++ src/advent_of_code/day_13.clj
@@ -12,10 +12,6 @@
 (def example "939
 7,13,x,x,59,x,31,19")
 
-(let [[earliest buses] (-> example parse)
-      bus (first buses)]
-  (- bus (mod 939 bus)))
-
 (defn part-1
   "Day 13 Part 1"
   [input]
@@ -29,7 +25,58 @@
 (part-1 example)
 (part-1 file)
 )
+
+; ********************* Part 2 ******************
+
+(defn parse-2 [input]
+  (let [[_ buses] (str/split-lines input)] 
+    (str/split buses #",")))
+
+(defn remove-x-keep-index [buses]
+  (vec (doall (for [bus buses
+                    :when (not= bus "x")]
+                [(read-string bus) (.indexOf buses bus)]))))
+
+(defn gcd [x y]
+  "greatest common divisor"
+  (let [a (mod x y)]
+    (if (= 0 a)
+      y
+      (gcd y a))))
+
+(defn lcm [x y]
+  "least common multiple"
+  (/ (* x y) (gcd x y)))
+
+(defn part-2-old
+  "Day 13 Part 2"
+  [input]
+  (let [buses (remove-x-keep-index (parse-2 input))
+        period (ffirst buses)]
+    (loop [t period]
+      (if (zero? 
+           (reduce + 
+                   (map 
+                    (fn [[moduli indx]] (mod (+ t indx) moduli)) 
+                    (rest buses))))
+        t
+        (recur (+ t period))))))
+
 (defn part-2
   "Day 13 Part 2"
   [input]
-  input)
+  (let [buses (remove-x-keep-index (parse-2 input))
+        period (ffirst buses)]
+    (loop [t period
+           step period
+           i 1] 
+      (if-let [[moduli indx] (get buses i)]
+        (if (zero? (mod (+ t indx) moduli))    
+          (recur t (lcm step moduli) (inc i))
+          (recur (+ t step) step  i))
+        t))))
+
+(comment         
+  (part-2 example) ;;   1068781
+  (part-2 file)
+)
